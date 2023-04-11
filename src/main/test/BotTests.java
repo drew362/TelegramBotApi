166f63f648557e2db99cfa7101c9b39b88e295af
Андрей Козин <agkozin@sberbank.ru>
2023-04-11 15:10:14 +0300

    апи тесты

--- src/main/test/BotTests.java
+++ src/main/test/BotTests.java
@@ -1,6 +1,9 @@
 import com.google.common.annotations.VisibleForTesting;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,9 +21,16 @@ public class BotTests {
                 .then()
                 .statusCode(200);
     }
-
+//https://api.telegram.org/bot1017809460:AAE4PO3os7ht_AraJ7J8MvYW6UTRMzl--3w/sendMessage?text=pi&chat_id=196918542
     @Test
     void checkTipesTest(){
+        List<DataUser> list = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL+"sendMessage?text=pi&chat_id=196918542")
+                .then().log().all()
+                .extract().body().jsonPath().getList("result",DataUser.class);
+
 
     }
 }
